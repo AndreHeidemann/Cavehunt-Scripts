895d0b799894c878194e0507a3da632e85e4e168
Andre Heidemann <a_n_heide@hotmail.com>
2024-07-07 20:37:59 -0300

    removido box5 e corrigido settings

--- wz7 - caitbox/settings.lua
+++ wz7 - caitbox/settings.lua
@@ -6,7 +6,7 @@ settings.itemsToBuy = {
     {id = 7643, name = "Ultimate Health Potion", amount = 0},
     {id = 239, name = "Great Health Potion", amount = 0},
     {id = 236, name = "Strong Health Potion", amount = 0}, -- Poções de mana
-    {id = 23373, name = "Ultimate Mana Potion", amount = 700},
+    {id = 23373, name = "Ultimate Mana Potion", amount = 250},
     {id = 238, name = "Great Mana Potions", amount = 0},
     {id = 237, name = "Strong Mana Potions", amount = 0},
 
@@ -14,10 +14,10 @@ settings.itemsToBuy = {
     {id = 23374, name = "Ultimate Spirit Potions", amount = 0},
     {id = 7642, name = "Great Spirit Potions", amount = 0}, -- Runas
     {id = 3191, name = "Great Fireball Rune", amount = 0},
-    {id = 3161, name = "Avalanche Rune", amount = 1000},
+    {id = 3161, name = "Avalanche Rune", amount = 700},
     {id = 3202, name = "ThunderStorm Rune", amount = 0},
     {id = 3160, name = "Ultimate Healing Rune", amount = 0},
-    {id = 3155, name = "Sudden Death Rune", amount = 200}
+    {id = 3155, name = "Sudden Death Rune", amount = 100}
 }
 
 -- Check to refill
@@ -34,7 +34,7 @@ settings.refillItems = {
 settings.caitBox = {
     enabled = true,
     maxAreaDistance = 8, -- Distância máxima para contar os monstros
-    monsterCountToContinue = 3, -- Quantidade de monstros minima para continuar para o proximo waypoint
+    monsterCountToContinue = 4, -- Quantidade de monstros minima para continuar para o proximo waypoint
     ignoreMonsters = {} -- Lista de monstros para ignorar
 }
 
